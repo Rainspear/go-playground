743d922ada106b07136375908f445724f42da1c5
Khang Phan <khang.phan@opswat.com>
2022-08-26 17:48:55 +0700

    construct deck

--- deck.go
+++ deck.go
@@ -4,6 +4,18 @@ import "fmt"
 
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Spades", "Diamond", "heart", "Club"}
+	cardValues := []string{"Ace", "One", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
+}
 func (d deck) print() {
 	for _, card := range d {
 		fmt.Println(card)
--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 package main
 
 func main() {
-	cards := deck{"1", "222", "2", "10"}
+	cards := newDeck()
 	cards.print()
 }
