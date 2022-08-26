9fbfd353940cfadd2f7f5233b7e179055edcad77
Khang Phan <khang.phan@opswat.com>
2022-08-27 04:25:44 +0700

    finish chapter 3

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -15,4 +18,19 @@ func TestNewDeck(t *testing.T) {
 	if d[len(d)-1] != "Four of Clubs" {
 		t.Errorf("Expect Four of Clubs, but got %s", d[len(d)-1])
 	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	d := newDeck()
+	d.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 20 {
+		t.Errorf("Expect Deck to have 20 elements, but got %d", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
 }
