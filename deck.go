bd4bf8ff906604d5693b6921535f991772ce5a54
Khang Phan <khang.phan@opswat.com>
2022-08-27 03:21:00 +0700

    write read bytes

--- deck.go
+++ deck.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 type deck []string
 
@@ -16,8 +20,32 @@ func newDeck() deck {
 	}
 	return cards
 }
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
 func (d deck) print() {
 	for _, card := range d {
 		fmt.Println(card)
 	}
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return os.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, error := os.ReadFile(filename)
+	cardString := ""
+	if error != nil {
+		fmt.Println("Error:", error)
+		os.Exit(1)
+	}
+	cardString = string(bs)
+	return deck(strings.Split(cardString, ","))
 }
--- main.go
+++ main.go
@@ -1,6 +1,14 @@
 package main
 
 func main() {
-	cards := newDeck()
+	cards := newDeckFromFile("my_cards.txt")
 	cards.print()
+	// cards := newDeck()
+	// cards.saveToFile("my_cards.txt")
+	// fmt.Println(cards.toString())
+	// hand, remainCards := deal(cards, 2)
+	// hand.print()
+	// fmt.Println("--------------------------------")
+	// remainCards.print()
+
 }
